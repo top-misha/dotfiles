956faa5627c0dcf2ef3806a4fb5e616fa083f9b0
top-misha <michgor715@gmail.com>
2026-06-08 00:01:40 +0500

    Add swaybar status script

--- sway/scripts/status_bar.sh
+++ sway/scripts/status_bar.sh
@@ -1,14 +1,37 @@
+
 #!/bin/bash
+block() 
+
+{
+	echo -n "{"
+	echo -n "\"full_text\":\"[$text]\","
+	echo -n "\"color\":\"$text_color\","
+	#echo -n "\"background\":\"$background_color\""
+	echo -n "\"separator\":\"false\","
+	echo -n "\"separator_block_width\":\"0\""
+	echo -n "},"
+}
+
+my_day()
+{
+	text=$(date +%^A)
+	text_color="#81a1c1"
+	background_color="#000000"
+	block
+}
+
+my_time() {
+	text=$(date +"%H:%M:%S")
+	text_color="#81a1c1"
+	block
+}
 echo '{ "version": 1}'
 	echo '[[]'	
 while true
 do
-	echo -n ",["
-	echo -n "{"
-	day=$(date  +%^A)
-	dateNow=$(date +"[%H:%M:%S][%d.%m.%y][${day}]")
-	echo -n "\"full_text\":\"$dateNow\""
-	echo -n "}"
-	echo -n "],]"
+	echo -n ',['
+	my_time
+	my_day	
+	echo -n ']'
 	sleep 1
 done
